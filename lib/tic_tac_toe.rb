0fea75336b6acaf8d56175946fb60187569dafc5
Andrew Davisson <akdavisson4@gmail.com>
2019-06-12 21:07:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,4 +70,21 @@ class TicTacToe
     end
   end
   
+  def won?(board)
+    WIN_COMBINATIONS.each do |win_combo|
+      win_index_1 = win_combo[0]
+      win_index_2 = win_combo[1]
+      win_index_3 = win_combo[2]
+    
+      pos_1 = board[win_index_1]
+      pos_2 = board[win_index_2]
+      pos_3 = board[win_index_3]
+    
+      if (pos_1 == "X" && pos_2 == "X" && pos_3 == "X") || (pos_1 == "O" && pos_2 == "O" && pos_3 == "O")
+        return win_combo
+      end
+    end
+    false
+  end
+  
 end
